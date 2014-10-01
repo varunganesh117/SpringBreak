2268d88135fe47a811fcde5a7c7dda8b005beb10
varunganesh91 <operationgenesis@gmail.com>
2014-10-01 21:54:31 +0800

    First commit

---
+++ src/main/java/springbreak/MainApp.java
@@ -0,0 +1,16 @@
+package springbreak;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+		
+		System.out.println(obj.getMessage());
+	}
+
+}
